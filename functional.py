f620b9be10f17bd3c78111c416969870929e141c
ssssplassssh <igorrosolovskij2016@gmail.com>
2023-06-09 23:07:36 +0300

    save txt json 4 field try except finally

--- functional.py
+++ functional.py
@@ -1,23 +1,71 @@
-import tkinter as tk
-from tkinter import ttk
 # Імпортуємо модуль tkinter для доступу до символу tk.END
+import tkinter as tk
+import json
 
-# Створюємо функцію send_button_clicked, яка виконується при натисканні кнопки "Send"
-def send_button_clicked(entry_text, frm, frk):
-    text = entry_text.get()
-    print('Input Text: ' + text)
+# Реалізація функції обробки поля з введеним значенням, отримання значення і виведення в консоль, очищення всього поля і заміна на нове значення "Новий текст"
 
-    entry_text.delete(0, tk.END)
+def field_processing(name_entry):
+    field_value = name_entry.get()
+    print('Input : ' + field_value)
+    name_entry.delete(0, tk.END)
+    name_entry.insert(0, "Новий текст")
+    return field_value
+
+# Створюємо функцію save_button_clicked, яка виконується при натисканні кнопки "Save"
+
+def save_button_clicked(service_entry, login_entry, email_entry, password_entry):
     
-    entry_text.insert(0, "Новий текст")
+    # Виклик функцій для обробки поля
     
-    save_button = ttk.Button(frk, text='Save password to file', command=lambda: save_password_to_file(text))
-    save_button.grid(column=0, row=0)
-    save_button = ttk.Button(frk, text='Save password to file')
-    save_button.grid(column=0, row=0)
+    service = field_processing(service_entry)  
+    login = field_processing(login_entry)  
+    email = field_processing(email_entry)
+    password = field_processing(password_entry)
+    
+    # Словник для зберігання у форматі .json 
+    data = {
+        "Service": service,
+        "Login": login,
+        "Email": email,
+        "Password": password
+    }
+    
+    # Зберігання у файл .json 
+    
+    save_data_to_file(data)
+    
+    # Зберігання у файл .txt
+    
+    # save_data_to_file(service, login, email, password)
+    
+# Зберігання у файл .json 
 
-def save_password_to_file(password):
-    filename = 'passwords.txt'
-    with open(filename, 'a') as file:
-        file.write(password + '\n')
-    print('Password saved to file:', password)
+def save_data_to_file(data):
+    filename = 'info.json' # file name
+    try: # перехоплює помилки при зберіганні
+        with open(filename, 'r') as file: # спроба прочитати файл
+            saved_data = json.load(file) # якщо файл існує і містить дійсний об'єкт, то він буде завантажений через json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError): # якщо файл не існує або містить недійсний json виникає помилка і файл набуває пустого списку
+        saved_data = []
+    
+    saved_data.append(data) # додавання даних у файл
+     
+    with open(filename, 'w') as file: # відкриття файлу для запису
+        json.dump(saved_data, file, indent=4) # зберігання у форматі json і встановлення відступів
+    
+    print(f"Data saved to file:\n{data}")
+        
+
+# Зберігання у файл .txt
+
+# def save_data_to_file(service, login, email, password):
+#     filename = 'info.txt'
+#     with open(filename, 'a') as file:
+#         file.write(service + '\n')
+#         file.write(login + '\n')
+#         file.write(email + '\n')
+#         file.write(password + '\n')
+#     print(f" Data saved to file: \n Service:  {service} \n Login:  {login} \n Email: {email} \n Password:  {password} \n")
+            
+            
+            
--- main.py
+++ main.py
@@ -1,52 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
 import functional as fn
-'''
-# Створення головного вікна
-root = tk.Tk()
-text_box = tk.Text()
-label_box = tk.Label(text='HEADER')
-label_box.pack()
-text_box.pack()
-
-# Створення рамок для елементів інтерфейсу
-frm = ttk.Frame(root, padding=10)
-frm.pack() # встановлюємо їх позицію у головному вікні 
-
-frs = ttk.Frame(root, padding=40)
-frs.pack() # встановлюємо їх позицію у головному вікні 
-
-# Створення мітки
-ttk.Label(frs, text="Це мітка", width=20).grid(column=1, row=2) # grid - розміщення в сітці
-
-entry = ttk.Entry(frs, text='It`s Entry', width=50)
-entry.grid(column=1, row=4)
-
-ttk.Button(frs, text="Send").grid(column=1, row=5)
-
-# Функція, яка викликається при натисканні кнопки "Send"
-def send_button_clicked():
-    # Отримати введений текст
-    text = entry.get()
-    print("Введений текст:", text)
-    
-    # Очистити поле введення,  видаляє весь текст з початку до кінця поля введення (включаючи обидва кінці)
-    entry.delete(0, tk.END)
-
-    # Вставити новий текст в поле введення
-    entry.insert(0, "Новий текст")
-
-# Прив'язка функції до кнопки "Send"
-ttk.Button(frs, text="Send", command=send_button_clicked).grid(column=1, row=5)
-
-# Створення кнопки
-ttk.Button(frs, text="Це кнопка").grid(column=1, row=3)
-
-# Створення мітки та кнопки в рамці
-ttk.Label(frm, text="Hello World!", foreground='white', background='black').grid(column=0, row=0)
-
-ttk.Button(frm, text="Quit",width=20,command=root.destroy).grid(column=1, row=0) # command=root.destroy - команда що закриває вікно
-'''
 
 # Створення головного вікна
 root = tk.Tk()
@@ -59,6 +13,8 @@ root.rowconfigure([0 ,1], minsize=250)
 header_box = tk.Label(text='Password Managing')
 header_box.pack()
 
+############################Frames#################################
+
 # Створюємо рамку frm для розміщення елементів інтерфейсу
 frm = ttk.Frame(root, padding=10, relief=tk.SUNKEN, borderwidth=5)
 frm.pack()
@@ -67,24 +23,64 @@ frm.pack()
 frk = ttk.Frame(root, padding=10, relief=tk.SUNKEN, borderwidth=5)
 frk.pack()
 
+############################# Buttons #############################
+
 # Створюємо кнопку "Exit" для закриття програми
+
 exit_btn = ttk.Button(frm, text='Exit', command=root.destroy)
 exit_btn.grid(column=4, row=10)
-
-# Створюємо поле введення entry_text для введення пароля.
-entry_text = ttk.Entry(frm, foreground='red')
-entry_text.grid(column=4, row=8)
 
 # Таким чином, коли кнопка "Send" натискається, виконується анонімна функція, яка потім викликає функцію functional.send_button_clicked з параметром entry_text.
 # Використання анонімної функції дозволяє передати параметри до функції functional.send_button_clicked без необхідності використання іменованої функції. Вона є зручним способом виклику функції з параметрами у випадку, коли потрібно передати додаткові аргументи або змінні до функції обробки події
 # Створюємо кнопку "Send" для виклику функції send_button_clicked з модуля functional, яка обробляє введений пароль
-send_btn = ttk.Button(frm, text='Send',command=lambda: fn.send_button_clicked(entry_text, frm, frk))
+
+send_btn = ttk.Button(frm, text='Save',command=lambda: fn.save_button_clicked(service_entry, login_entry, email_entry, password_entry))
 send_btn.grid(column=4, row=9)
 
-# Створюємо мітку entry_text_label для підпису до поля введення пароля
-entry_text_label = ttk.Label(frm, text='Input Password: ')
-entry_text_label.grid(column=2, row=8)
 
+
+#############################Entries###############################
+
+# Створюємо поле введення entry_text для введення login.
+
+service_entry = ttk.Entry(frm, foreground='black')
+service_entry.grid(column=4, row=5)
+
+login_entry = ttk.Entry(frm, foreground='green')
+login_entry.grid(column=4, row=6)
+
+# Створюємо поле введення email_entry для введення email.
+
+email_entry = ttk.Entry(frm, foreground='blue')
+email_entry.grid(column=4, row=7)
+
+# Створюємо поле введення password_entry для введення password.
+
+password_entry = ttk.Entry(frm, foreground='red')
+password_entry.grid(column=4, row=8)
+
+
+#################################Labels############################
+
+# Створюємо мітку login_label для підпису до поля введення service
+
+service_label = ttk.Label(frm, text='Input Service: ' )
+service_label.grid(column=2, row=5)
+
+# Створюємо мітку login_label для підпису до поля введення login
+
+login_label = ttk.Label(frm, text='Input Login: ' )
+login_label.grid(column=2, row=6)
+
+# Створюємо мітку email_label для підпису до поля введення email
+
+email_label = ttk.Label(frm, text='Input Email: ' )
+email_label.grid(column=2, row=7)
+
+# Створюємо мітку password_label для підпису до поля введення password
+
+password_label = ttk.Label(frm, text='Input Password: ')
+password_label.grid(column=2, row=8)
 
 # Запуск головного циклу Tkinter
 root.mainloop()
